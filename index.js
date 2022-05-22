513da11e20257859b324c1eeb925cd46c6a89447
John Doe <hamedhasan422421@gmail.com>
2022-05-22 16:05:35 +0300

    users login and signIn go to signIn send user to db

--- index.js
+++ index.js
@@ -25,7 +25,21 @@ async function run() {
   
 
 
+//  users login and signIn go to signIn send user to db with put
+app.put('/user/:email', async (req, res) => {
+  const email = req.params.email;
+  const user = req.body;
+  const filter = { email: email };
+  const options = { upsert: true };
+  const updateDoc = {
+    $set: user,
+  };
+  const result = await userCollection.updateOne(filter, updateDoc, options);
+  // res.send(result);
+  const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+  res.send({ result, token });
 
+})
 
 
    
