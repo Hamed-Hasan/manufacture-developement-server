2655a85945bdfbe3794740f9916bbc620c4b479d
John Doe <hamedhasan422421@gmail.com>
2022-05-28 13:21:14 +0300

    update

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ async function run() {
     }
 
 // create payment method for stripe 
-app.post('/create-payment-intent', async(req, res) =>{
+app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
   const service = req.body;
   const price = service.price;
   const amount = price*100;
